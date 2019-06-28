c43a1a59e8312dd55f33f9b8492539a417f0f573
mhtvsSFrpHdE <2bemperor47@gmail.com>
2019-06-28 23:58:03 +0800

    Remove depth=1

--- AutoEqUpdate-Update.sh
+++ AutoEqUpdate-Update.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -v
 
 # The first time download, if failed that means already downloaded
-git clone --depth=1 --branch=master https://github.com/jaakkopasanen/AutoEq.git $autoEqInstPath
+git clone --branch=master https://github.com/jaakkopasanen/AutoEq.git $autoEqInstPath
 cd $autoEqInstPath
 # Try to remove any local commit(to prevent if someone stupid enough to do this)
 git reset --hard
 # Get latest update
-git fetch --depth=1
+git fetch
 # GIT'EM ALL
 git clean -fdx
 
