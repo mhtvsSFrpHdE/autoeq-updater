ecedf792ace4f193d7da6135476725d8171d8842
mhtvsSFrpHdE <2bemperor47@gmail.com>
2019-07-19 09:31:46 +0800

    Add, this method should do the thing
    Switch to master branch
    Remove files added after recent commit
    Remove files doesn't belong to AutoEq
    Check update
    Fast forward

--- AutoEqUpdate-Update.sh
+++ AutoEqUpdate-Update.sh
@@ -4,8 +4,11 @@
 git clone --branch=master https://github.com/jaakkopasanen/AutoEq.git $autoEqInstPath
 cd $autoEqInstPath
 # Try to remove any local commit(to prevent if someone stupid enough to do this)
+git checkout master
 git reset --hard
+git clean -fdx
 # Get latest update
 git fetch
+git pull
 
 exit
